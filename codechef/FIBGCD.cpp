41b111c9ebda7a8b1f23249995ba262350ac2d23
Akirato <nurendra.choudhary@research.iiit.ac.in>
2015-01-17 19:01:16 +0530

    fibonacci

--- codechef/FIBGCD.cpp
+++ codechef/FIBGCD.cpp
@@ -1,8 +1,6 @@
 #include<iostream>
 using namespace std;
 int fibo[1000000]={0};
-fibo[0]=1;
-fibo[1]=1;
 int gcd(int a,int b)
 {
 	while(b!=0)
@@ -15,11 +13,19 @@ int gcd(int a,int b)
 }
 int fib(int n)
 {
+	fibo[0]=1;
+        fibo[1]=1;
 	if(n<3)
-           return fibo[0];
-        else if(fibo[n]!=0)
-           return fibo[n];
-        else if(fibo[n-1]!=0 and 
+            return fibo[0];
+        else if(fibo[n-1]!=0)
+            return fibo[n-1];
+        else if(fibo[n-2]!=0 && fibo[n-3]!=0)
+            fibo[n-1]=(fibo[n-2]+fibo[n-3])%10000000007;
+        else if(fibo[n-2]!=0)
+            fibo[n-1]=(fibo[n-2]+fib(n-3))%10000000007;
+        else
+            fibo[n-1]=(fib(n-2)+fib(n-3))%10000000007;
+        return fibo[n-1];
 }
 int main()
 {
@@ -29,6 +35,8 @@ int main()
 	{
 		int a,b;
 		cin>>a>>b;
+                if(a==0 && b==0)
+                    cout<<"0"<<endl;
 		cout<<fib(gcd(a,b))<<endl;
 	}
 	return 0;
